19c82c38e3c97d86641b6e377d2c5fe6aaf3efdb
Luke Van Delden <vandelden.luke@gmail.com>
2023-04-01 01:45:06 -0500

    messing with loops

--- test.php
+++ test.php
@@ -5,6 +5,13 @@
     <title>Title</title>
 </head>
 <body>
+<form action="test.php" method="post">
+    <label for="counter">Counter</label>
+    <input type="number" name="counter">
+    <label for="age">Age</label>
+    <input type="number" name="age">
+    <input type="submit">
+</form>
 <?php
 $array = ["Test1", 25, "TEST2"];
 echo "<h1> Hello Kara you sexy thing</h1>";
@@ -12,17 +19,35 @@ echo "<h1> The first array element is $array[0]";
 echo "<h1> The second array element is $array[1]";
 echo "<h1> The third array element is $array[2]";
 echo "<br>";
-$counter = 5;
 
 function doSomething($counter){
-    if ($counter > 0){
+if (!empty($_POST['counter'])){
+    if ($counter > 0) {
         echo "Counter = $counter <br>";
         $counter--;
         doSomething($counter);
     }
+} else{
+    echo "<h1>Enter in the number of repeats</h1>";
+}
 }
 
-doSomething($counter);
+$counter = 0;
+if (!empty($_POST['counter'])){
+    $counter = $_POST["counter"];
+}
+
+    doSomething($counter);
+if (isset($_POST['age'])) {
+    $age = $_POST['age'];
+    echo "Age is $age";
+}
+    $x = 5;
+do {
+    echo "x = $x <br>";
+    $x--;
+
+} while ($x > 0)
 ?>
 </body>
 </html>
